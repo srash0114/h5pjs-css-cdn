9881732d8a6b556d95c406d6a52ab863b8f313f6
srash0114 <thai25800852@gmail.com>
2025-07-31 15:54:41 +0700

    push

--- dist/h5p-interactive-video.js
+++ dist/h5p-interactive-video.js
@@ -4402,7 +4402,6 @@
     // Check if in fullscreen and on iOS, and exit fullscreen if an interactive question is about to be shown
     if (
         isIOS &&
-        H5P.isFullscreen &&
         void 0 !== this.nextInteractionToShow &&
         this.interactions[this.nextInteractionToShow].isQuestion &&
         this.interactions[this.nextInteractionToShow].isQuestion()
