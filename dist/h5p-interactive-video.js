be89e58f218d2896315956dcd0730702685eb8b8
srash0114 <thai25800852@gmail.com>
2025-07-31 13:37:51 +0700

    push

--- dist/h5p-interactive-video.js
+++ dist/h5p-interactive-video.js
@@ -525,6 +525,7 @@
             e.currentState !== H5P.InteractiveVideo.SEEKING &&
             P(!0),
           B("<div/>", { class: "h5p-touch-area" }).appendTo(n),
+          video.webkitExitFullscreen()
           B("<div/>", { class: "h5p-interaction-button", style:"width: 50px; height: 50px; border-radius: 50%;background-color: #9c27b0;display: flex;align-items: center;justify-content: center;font-size: 32px;color: white; cursor: pointer;"}).appendTo(n),
           e.editor &&
             n
