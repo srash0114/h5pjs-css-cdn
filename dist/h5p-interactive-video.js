63238df438f46989cb12b46c6b3461700a161b6c
srash0114 <thai25800852@gmail.com>
2025-09-15 16:30:39 +0700

    fix css

--- dist/h5p-interactive-video.js
+++ dist/h5p-interactive-video.js
@@ -4573,7 +4573,7 @@
       const CurrentUrl = window.location.href;
       const parts = CurrentUrl.split("/").filter(Boolean);
       const CourseId = parts[1];
-      const sequenceId = url.searchParams.get("sequence_id");
+      const sequenceId = CurrentUrl.searchParams.get("sequence_id");
       console.log("Current URL...", CurrentUrl);
       console.log("sequence_id...", sequenceId);
       console.log("CourseId...", CourseId);
