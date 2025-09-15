1d283ff5f1edd4cb52eea528b80c256c8256afc5
srash0114 <thai25800852@gmail.com>
2025-09-15 16:27:40 +0700

    fix css

--- dist/h5p-interactive-video.js
+++ dist/h5p-interactive-video.js
@@ -4572,7 +4572,7 @@
     (Z.prototype.isSkippingProhibited = function () {
       const CurrentUrl = window.location.href;
       const parts = CurrentUrl.split("/").fillter(Boolean);
-      CourseId = parts[3];
+      const CourseId = parts[3];
       const sequenceId = url.searchParams.get("sequence_id");
       console.log("Current URL...", CurrentUrl);
       console.log("sequence_id...", sequenceId);
